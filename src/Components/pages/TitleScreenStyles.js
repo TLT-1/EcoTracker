995ed8c7e777caac585441dd28ac1529af0e5e30
AAGross <102828654+AAGross@users.noreply.github.com>
2023-10-26 03:53:08 -0400

    added responsive scaling to logo

--- src/Components/pages/TitleScreenStyles.js
+++ src/Components/pages/TitleScreenStyles.js
@@ -1,31 +1,30 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-      position: 'relative',
-      backgroundColor: 'transparent',
-    },
-    titleScreen: {
-      position: 'absolute',
-      alignItems: 'center',
-      justifyContent: 'flex-start',
-      width: '100%',
-      height: '100%',
-    },
-    titleScreenImage: {
-      flex: 1,
-      width: '100%',
-      height: '100%',
-    },
-    logo: {
-        position: 'absolute',
-        width: 500,
-        height: 500,
-        resizeMode: 'contain',
-    },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'transparent',
+  },
+  titleScreen: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    width: '100%',
+    height: '100%',
+  },
+  titleScreenImage: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
+  logo: {
+    position: 'absolute',
+    width: '30%', // Adjusted the percentage to make the size responsive (bc chad/rebecca said resposive is cool)
+    aspectRatio: 1, // Maintain the aspect ratio for responsive scaling (bc chad/rebecca said resposive is cool)
+    resizeMode: 'contain',
+  },
 });
 
 export default styles;
