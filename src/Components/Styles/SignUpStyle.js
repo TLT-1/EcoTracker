cd5454dcc2bffabd769548766fbfc254b8757b7f
TLT-1 <59715758+TLT-1@users.noreply.github.com>
2023-11-03 13:58:10 -0400

    Sign up page with proper fields created

---
+++ src/Components/Styles/SignUpStyle.js
@@ -0,0 +1,66 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "transparent",
+    },
+    titleScreen: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "flex-start",
+        width: "100%",
+        height: "100%",
+    },
+    titleScreenImage: {
+        flex: 1,
+        width: "100%",
+        height: "100%",
+    },
+    logo: {
+        position: "absolute",
+        width: "25%", // Adjusted the percentage to make the size responsive (bc chad/rebecca said responsive is cool)
+        aspectRatio: 1, // Maintain the aspect ratio for responsive scaling (bc chad/rebecca said responsive is cool)
+        resizeMode: "contain",
+        top: "0%",
+    },
+    inputContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#4CAF50' // Greenish background color
+    },
+    input: {
+        width: '180%',
+        height: 50,
+        padding: 10,
+        margin: 10,
+        borderColor: 'blue',
+        borderWidth: 1,
+        borderRadius: 5,
+        backgroundColor: 'white',
+        borderRadius: 15,
+    },
+    button: {
+        width: 170,
+        height: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#8B4513',
+        borderRadius: 5,
+        marginTop: 10,
+        borderRadius: 15,
+    },
+    buttonText: {
+        color: 'black',
+        fontSize: 18,
+    },
+    linkText: {
+        color: 'blue',
+        textDecorationLine: 'underline', // Optional: Adds an underline for the clickable text
+    },
+});
+
+export default styles;
--- src/Components/pages/SignUp.js
+++ src/Components/pages/SignUp.js
@@ -1,7 +1,145 @@
-import React from 'react';
-import '../../App.css';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, Image, Animated, Easing, Button, TextInput, Alert, TouchableOpacity } from "react-native";
+import styles from "../Styles/SignUpStyle";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import axios from 'axios';
 
 
-export default function SignUp() {
-    return <h1 className='sign-up'>SIGN UP</h1>;
+
+
+function LogIn() {
+    const [scaleValue] = useState(new Animated.Value(1));
+
+    useEffect(() => {
+        const pulseAnimation = Animated.sequence([
+            Animated.timing(scaleValue, {
+                toValue: 1.15,
+                duration: 1500,
+                easing: Easing.ease,
+                useNativeDriver: true,
+            }),
+            Animated.timing(scaleValue, {
+                toValue: 1,
+                duration: 1500,
+                easing: Easing.ease,
+                useNativeDriver: true,
+            }),
+        ]);
+
+        Animated.loop(pulseAnimation).start();
+    }, [scaleValue]);
+
+    const [first, setFirst] = useState('');
+    const [last, setLast] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    
+    
+    //console.log(username, password)
+
+    const handleLogin = async () => {
+        try {
+            // Make POST request using axios with data
+            const response = await axios({
+                method: 'POST',
+                url: 'http://localhost:5000/signup',
+                headers: { 'Content-Type': 'application/json' },
+                data: {
+                    first: first,
+                    last: last,
+                    email: email,
+                    password: password,
+                }
+            });
+
+            console.log(response.data);  // Print out the response data
+        } catch (error) {
+            console.error(error);
+        }
+    }; 
+
+    handleLogin();
+
+
+
+    return (
+
+
+        < View style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100vh', // full viewport height
+            background: 'linear-gradient(to bottom, rgb(27, 135, 14), rgb(1, 35, 5) 100%)'
+        }}>
+
+            <Animated.Image
+                source={require("../../../assets/ecoTrackLogosu.png")}
+                style={[styles.logo, { transform: [{ scale: scaleValue }] }]}
+            />
+
+
+
+            <View style={styles.container}>
+                <TextInput
+                    style={styles.input}
+                    placeholder="First Name"
+                    value={first}
+                    onChangeText={text => setFirst(text)}
+                    autoCapitalize="none"
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder="Last Name"
+                    value={last}
+                    onChangeText={text => setLast(text)}
+                    autoCapitalize="none"
+                />
+                <br></br>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Email"
+                    value={email}
+                    onChangeText={text => setEmail(text)}
+                    secureTextEntry={true}
+                    autoCapitalize="none"
+                />
+                <br></br>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Password"
+                    value={password}
+                    onChangeText={text => setPassword(text)}
+                    secureTextEntry={true}
+                    autoCapitalize="none"
+                />
+                <br></br>
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={async () => {
+                        navigation.navigate('Title');
+                    }}
+                >
+                    <Text style={styles.buttonText}>Sign In</Text>
+                </TouchableOpacity>
+            </View>
+
+
+            <Text style={styles.text}>Have an Account? </Text>
+            <TouchableOpacity onPress={() => navigation.navigate('LogIn')}>
+                <Text style={styles.linkText}>Sign In</Text>
+            </TouchableOpacity>
+
+
+        </View >
+
+
+
+
+    );
 }
+export default LogIn;
+
+
+
