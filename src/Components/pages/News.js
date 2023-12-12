a8304b39160f826522734617e82539a60989692e
AAGross <102828654+AAGross@users.noreply.github.com>
2023-12-12 18:09:58 -0500

    minor fixes

--- src/Components/pages/News.js
+++ src/Components/pages/News.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, FlatList, StyleSheet, ImageBackground, TouchableOpacity, Image } from 'react-native';
+import { View, ScrollView, Text, FlatList, StyleSheet, ImageBackground, TouchableOpacity, Image } from 'react-native';
 import axios from 'axios';
 import Navbar from '../Navbar';
 import Footer from '../Footer';
@@ -18,13 +18,13 @@ function News() {
             try {
                 const response = await axios.get('https://newsapi.org/v2/everything', {
                     params: {
-                        q: 'climate change, environment',
+                        q: '"climate change", "environment", "climate crisis"',
                         sortBy: 'publishedAt',
                         apiKey: 'bde5b060f669431985e03cadc068dd43'
                     }
                 });
 
-                const excludedDomains = ['subscriber.politicopro.com', 'www.vox.com', 'www.sciencedaily.com', 'www.forbes.com', 'www.planetizen.com', 'press.un.org', 'www.rigzone.com', 'www.globenewswire.com', 'www.marketscreener.com'];
+                const excludedDomains = ['subscriber.politicopro.com', 'www.vox.com', 'www.sciencedaily.com', 'www.forbes.com', 'www.planetizen.com', 'press.un.org', 'www.rigzone.com', 'www.globenewswire.com', 'www.marketscreener.com', 'www.defense.gov', 'removed.com',];
 
                 const filteredArticles = response.data.articles.filter(article => {
                     const articleDomain = new URL(article.url).hostname;
@@ -66,20 +66,22 @@ function News() {
             <Navbar />
             <ImageBackground source={require("../../../assets/ecoBackgroundChristmas.png")} style={{ flex: 1, ...styles.container, overflow: 'hidden' }}>
                 {news.length > 0 ? (
-                    <FlatList
-                        data={news}
-                        renderItem={renderItem}
-                        keyExtractor={item => item.url}
-                        numColumns={2}
-                        contentContainerStyle={styles.listContainer}
-                        ListHeaderComponent={
-                            <Text style={{ fontSize: 50, fontWeight: 'bold', color: 'white', textAlign: 'center' }}>Current News</Text>
-                        }
-                    />
+                    <ScrollView>
+                        <Text style={{ fontSize: 50, fontWeight: 'bold', color: 'white', textAlign: 'center' }}>Current News</Text>
+                        <FlatList
+                            data={news}
+                            renderItem={renderItem}
+                            keyExtractor={item => item.url}
+                            numColumns={2}
+                            contentContainerStyle={styles.listContainer}
+
+                        />
+                    </ScrollView>
                 ) : (
                     <View style={styles.placeholderContainer}>
                         <Text style={styles.placeholderText}>No news available</Text>
                     </View>
+
                 )}
                 <Modal
                     isOpen={modalIsOpen}
@@ -105,7 +107,7 @@ function News() {
                         }
                     }}
                 >
-                    <div style={{ width: 'calc(100% - 4px)', height: 'calc(100% - 4px)', boxShadow: '0 6px 20px 10px rgba(0, 0, 0, 0.8)', border: '2px solid #000' }}>
+                    <div style={{ width: '100%', height: '100%', boxShadow: '0 6px 20px 10px rgba(0, 0, 0, 0.8)', border: '2px solid #000' }}>
                         <iframe src={modalUrl} width="100%" height="100%"></iframe>
                     </div>
                     <button onClick={closeModal} style={{ position: 'absolute', top: 0, right: 0 }}>X</button>
@@ -114,28 +116,29 @@ function News() {
             <Snowfall style={{}} snowflakeCount={250} />
             <Footer style={{ height: 18 }} navigation={navigation} />
         </View>
+
     );
 }
 
 const styles = StyleSheet.create({
     listContainer: {
-        alignItems: 'center', // Center items horizontally
-        paddingLeft: 100,
+        alignItems: 'center',
+        paddingLeft: 200,
     },
     newsItem: {
         margin: 10,
         padding: 10,
         backgroundColor: '#fff',
         borderRadius: 10,
-        width: '45%', // Adjust the width to fit more items in a row
+        width: '40%',
         alignSelf: 'center',
-        borderColor: "#062a52", // Change the border color here
+        borderColor: "#062a52",
         borderWidth: 2,
         boxShadow: '10px 5px 10px 5px rgba(0.5, 0.5, 0.5, 0.75)',
     },
     image: {
-        width: '100%', // Set the width to 100% of the parent container
-        height: 200, // Set a specific height
+        width: '100%',
+        height: 200,
     },
     title: {
         fontSize: 18,
@@ -143,10 +146,6 @@ const styles = StyleSheet.create({
     },
     source: {
         fontSize: 16,
-    },
-    url: {
-        fontSize: 14,
-        color: 'blue',
     },
 });
 
